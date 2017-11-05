b095cd15e7564abb4fd25e14005c155b43d3846d
Olivesan <jacob.barlowellevsen@gmail.com>
2017-11-05 11:58:09 +1000

    Add files via upload
    Version 1

---
+++ core/inclusions.php
@@ -0,0 +1,4 @@
+<LINK href = "css/reset.css" rel = "stylesheet" type = "text/css">
+<LINK href = "css/style.css" rel = "stylesheet" type = "text/css">
+
+<SCRIPT src = "core/js/ball.js"></SCRIPT>
---
+++ core/js/ball.js
@@ -0,0 +1,38 @@
+var colors = [
+	"white",
+	"red",
+	"orange",
+	"yellow",
+	"lightgreen",
+	"green",
+	"blue",
+	"lightblue",
+	"purple",
+	"salmon"
+]
+
+function Ball(x, y, number)
+{
+	this.position =	{
+		'x': x,
+		'y': y
+	};
+
+	this.number = number;
+	this.radius = 5;
+
+	this.color = colors[number];
+
+	//console.log(this.number + ": " + this.color);
+
+	this.Draw = function()
+	{
+		c.beginPath();
+		c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+		c.fillStyle = this.color;
+		c.fill();
+		c.strokeStyle = "#FFF;"
+		c.lineWidth = 2;
+		c.stroke();
+	}
+}
